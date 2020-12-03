a27a1b13047906bc026ef82119bcc855dca07a13
Ioan-Mihail Stan <ioan-mihail.stan@cern.ch>
2020-12-03 17:28:46 +0200

    dummy validation ex8

--- checker/lab09/checker.sh
+++ checker/lab09/checker.sh
@@ -43,13 +43,80 @@ function checker_ex6(){
 
 	# in order to work, we have to avoid adding -i eth0 as it is currently described in teacher's log
 	return 0
-	
 }
+
+# ex 7 manual
+function helper_get_telnet_remote_host(){
+
+	if [ ! "$#" -ge "2" ]; then
+    		echo "Illegal number of parameters"
+		return -1
+     	fi
+
+     	if  [ ! "$1" == "red" ] && [ ! "$1" == "green" ] && [ ! "$1" == "blue" ] && [ ! "$1" == "host" ]; then
+		return -2
+	fi
+
+     	if  [ ! "$2" == "red" ] && [ ! "$2" == "green" ] && [ ! "$2" == "blue" ] && [ ! "$2" == "host" ]; then
+		return -2
+	fi
+
+	
+	#TODO check if $2 exists and it is a number
+	if [ -z $3 ]; then rmt_port=23; else rmt_port=$3; fi
+
+	remote_host=''
+	if [ "$1" != "host" ]; then
+	  remote_host=`docker exec -t --user=student mn.$1 /bin/bash \
+		-c "{
+	             sleep 2
+		     echo 'student' 
+		     sleep 1
+		     echo 'student' 
+		     sleep 1
+		     echo '/bin/hostname'
+		     sleep 1
+		     echo 'exit'
+		     } | timeout 10 telnet $2 $rmt_port 2> /dev/null | grep -i "/bin/hostname" -A 1 \
+			     | tail -n 1 | tr -dc '[:print:]'"`
+	
+        else
+		   remote_host=`{
+                     sleep 2
+                     echo "student"
+                     sleep 1
+                     echo "student"
+                     sleep 1
+                     echo "/bin/hostname"
+                     sleep 1
+                     echo "exit"
+                     } | timeout 10 telnet $2 $rmt_port 2> /dev/null | grep -i "/bin/hostname" -A 1 \
+                             | tail -n 1` 
+
+	fi
+	echo $remote_host
+}
+
+function checker_ex8(){
+	# dummy approach
+
+	# red to green via host
+	#helper_get_telnet_remote_host red host 20023
+	# red to blue via host
+	#helper_get_telnet_remote_host red host 30023
+	# green to red via host
+	rmt_host=$(helper_get_telnet_remote_host green host 10023)
+	if [ ! -z $rmt_host ] && [ $rmt_host == "red" ]; then return 0; fi
+	return 1;
+
+
+}
+
 
 function main(){
 	#todo investigate err: failed to resize tty, using default size
 	declare -a checker_modules=("checker_ex1" "checker_ex4" "checker_ex5"\
-		"checker_ex6")
+		"checker_ex6" "checker_ex8")
 	for val in ${checker_modules[@]}; do
 		echo  -n "$val ####################################################### ";
 		if $val; then
