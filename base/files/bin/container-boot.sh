4918d32867dc871d54be06a0d2e00bec8c30eb77
Stancu Florin <niflostancu@gmail.com>
2020-12-06 18:22:21 +0200

    base: fix locales & container environment

--- base/files/bin/container-boot.sh
+++ base/files/bin/container-boot.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 # Runs at container boot time
 
+# import container's environment variables from systemd
+. <(xargs -0 bash -c 'printf "export %q\n" "$@"' -- < /proc/1/environ)
+
+# save RL_PS1_FORMAT to profile
+printf "RL_PS1_FORMAT=\"%s\"\n" "$RL_PS1_FORMAT" > /etc/profile.d/rl.sh
+
 sysctl -w net.ipv6.conf.all.disable_ipv6=0
 
